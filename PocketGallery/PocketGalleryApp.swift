263bbed46ce10e31257807c9e466c28194828682
axfng <alexlfeng22@gmail.com>
2025-01-16 23:28:06 -0500

    Initial Commit

---
+++ PocketGallery/PocketGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketGalleryApp.swift
+//  PocketGallery
+//
+//  Created by alfeng on 1/16/25.
+//
+
+import SwiftUI
+
+@main
+struct PocketGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
